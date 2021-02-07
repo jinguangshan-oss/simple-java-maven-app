dfcd505bc05d6be4ed5b1b609c24ab9e1ec2dbee
jinguangshan <qiaozhi31002@163.com>
2021-02-07 10:56:25 +0800

    20210207

--- src/main/java/com/jgs/controller/HostController.java
+++ src/main/java/com/jgs/controller/HostController.java
@@ -10,6 +10,6 @@ public class HostController {
 
     @RequestMapping(path = "/host", method = RequestMethod.GET)
     String home() {
-        return "http://47.103.199.109:8811/";
+        return "//47.103.199.109:8811";
     }
 }
