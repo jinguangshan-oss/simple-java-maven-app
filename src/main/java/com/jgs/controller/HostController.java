98e681d6161fb090f324c7df9d43213058107eba
jinguangshan <qiaozhi31002@163.com>
2021-02-09 16:49:28 +0800

    20210209

--- src/main/java/com/jgs/controller/HostController.java
+++ src/main/java/com/jgs/controller/HostController.java
@@ -53,35 +53,5 @@ public class HostController {
         return "hello-aa" + WebUtils.getIP();
     }
 
-    public static void main(String[] args) {
-        String host = "47.103.199.109";
-        int port = 8000;
-
-        String url = "http://47.103.199.109/host";
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        Map bodyMap = new HashMap();
-        bodyMap.put("merchantId","NYG88");
-        bodyMap.put("currentTime","1608262656500");
-        bodyMap.put("beginTime","2020-12-18 11:49:00");
-        bodyMap.put("sign","f3b19b12450fa73269e37302a77e955a");
-        HttpEntity<Object> httpEntity = new HttpEntity<>(bodyMap, headers);
-
-
-        //初始化代理
-        SocketAddress address = new InetSocketAddress(host, port);
-        Proxy proxy = new Proxy(Proxy.Type.HTTP, address);
-
-        //初始化客户端http请求工厂
-        SimpleClientHttpRequestFactory httpRequestFactory = new SimpleClientHttpRequestFactory();
-        httpRequestFactory.setReadTimeout(10000);
-        httpRequestFactory.setConnectTimeout(10000);
-        httpRequestFactory.setProxy(proxy);
-        RestTemplate restTemplate = new RestTemplate(httpRequestFactory);
-
-        ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.GET, httpEntity, String.class);
-        System.out.println(responseEntity.getBody());
-    }
 }
   
